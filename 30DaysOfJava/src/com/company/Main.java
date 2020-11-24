cf61af593cd08c13cab239bb07fe5c586ccfd5e6
DevTechPro <officialdavidacc@gmail.com>
2020-11-24 16:34:30 +0300

    Added working with linked List and reading from file examples

--- 30DaysOfJava/src/com/company/Main.java
+++ 30DaysOfJava/src/com/company/Main.java
@@ -3,22 +3,37 @@ package com.company;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-	  Scanner in  = new Scanner(new File("students.text"));
+        readFromFile();
+        workingWithCollection();
+    }
+
+    private static void readFromFile() throws FileNotFoundException {
+        Scanner in = new Scanner(new File("students.text"));
         List<String> students = new ArrayList<String>();
-        while (in.hasNextLine()){
-           students.add(in.nextLine());
+        while (in.hasNextLine()) {
+            students.add(in.nextLine());
         }
 
-        for(int i = 0; i < students.size(); i++){
-            System.out.println(students.get(i));
+        for (String student : students) {
+            System.out.println(student);
         }
 
-	  in.close();
+        in.close();
+    }
+
+    private static void workingWithCollection(){
+        LinkedList<String> names = new LinkedList<String>();
+        names.push("David");
+        names.push("Beatrice");
+
+        System.out.println(names.remove());
+        System.out.println(names.remove());
     }
 }
