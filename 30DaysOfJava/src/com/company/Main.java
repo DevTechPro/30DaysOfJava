7e267e0d1c4b15150133f0d00f7fb8f62e60e82a
DevTechPro <officialdavidacc@gmail.com>
2020-11-24 15:30:35 +0300

    Simple colletion

--- 30DaysOfJava/src/com/company/Main.java
+++ 30DaysOfJava/src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+	  Scanner in  = new Scanner(new File("students.text"));
+        List<String> students = new ArrayList<String>();
+        while (in.hasNextLine()){
+           students.add(in.nextLine());
+        }
+
+        for(int i = 0; i < students.size(); i++){
+            System.out.println(students.get(i));
+        }
+
+	  in.close();
     }
 }
